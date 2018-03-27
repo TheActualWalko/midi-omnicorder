c3fe937dbc872139df29beb2dd9c15866527b6d5
Sam Watkinson <sam.ws.watkinson@gmail.com>
2018-03-26 19:23:37 -0700

    build out of sub-folder (oops)

--- build-packages.sh
+++ build-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-electron-packager . --platform=darwin --arch=x64 --out=packages --overwrite && electron-packager . --platform=win32 --arch=ia32 --out=packages --overwrite
+electron-packager ./app --platform=darwin --arch=x64 --out=packages --overwrite && electron-packager ./app --platform=win32 --arch=ia32 --out=packages --overwrite
