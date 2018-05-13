5fb36527365d6ec8e4632b93929aa8403a839a1e
Sam Watkinson <sam.ws.watkinson@gmail.com>
2018-05-13 11:09:33 -0400

    windows support

--- build-packages.sh
+++ build-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # add this to enable windows:
-# && electron-packager ./app --platform=win32 --arch=ia32 --out=packages --overwrite && zip -r website-downloads/MIDICatch-win32-ia32.zip packages/MIDICatch-win32-ia32
+# && electron-packager ./app --platform=win32 --arch=x64 --out=packages --overwrite && zip -r website-downloads/MIDICatch-win32-x64.zip packages/MIDICatch-win32-x64
 
 rm website-downloads/* && electron-packager ./app --platform=darwin --arch=x64 --out=packages --overwrite && zip -r website-downloads/MIDICatch-darwin-x64.zip packages/MIDICatch-darwin-x64
